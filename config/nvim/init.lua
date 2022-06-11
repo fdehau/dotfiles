6ab384d61932a26889379de855c82d1a7b7028e8
Florian Dehau <work@fdehau.com>
2022-06-11 20:40:55 +0200

    feat(nvim): make `fzf.vim` use nvim colorscheme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -186,6 +186,21 @@ require("packer").startup(function()
             vim.fn["fzf#install"]()
         end,
         config = function()
+            vim.g.fzf_colors = {
+                fg      = {'fg', 'Normal'},
+                bg      = {'bg', 'Normal'},
+                hl      = {'fg', 'Comment'},
+                ['fg+'] = {'fg', 'CursorLine', 'CursorColumn', 'Normal'},
+                ['bg+'] = {'bg', 'CursorLine', 'CursorColumn'},
+                ['hl+'] = {'fg', 'Statement'},
+                info    = {'fg', 'PreProc'},
+                border  = {'fg', 'Ignore'},
+                prompt  = {'fg', 'Conditional'},
+                pointer = {'fg', 'Exception'},
+                marker  = {'fg', 'Keyword'},
+                spinner = {'fg', 'Label'},
+                header  = {'fg', 'Comment'},
+            }
             keymap("n", "<Leader>f", ":Files <CR>", {silent = true})
             keymap("n", "<Leader>o", ":Buffers <CR>", {silent = true})
             keymap("n", "<Leader>l", ":BLines <CR>", {silent = true})
